63654802eed719586a56102c54bf0414bf3c837d
Suren Ispiryan <surenispiryan@Surens-MacBook-Pro.local>
2023-06-12 20:07:42 +0400

    chat simple visual created

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <div class="row">
+<!-- header -->
       <div class="col-md-3">
         <h3>Hi {{ this.authUser.name }}</h3>
       </div>
@@ -13,7 +14,7 @@
         <button class="btn btn-danger" @click="logout">logout</button>
       </div>
     </div>
-
+<!-- users -->
     <div class="row pt-5">
       <div class="col-md-4 users-list p-4">
         <div class="user-item"  v-bind:key="index" v-for="(item, index) in this.users">
@@ -21,9 +22,22 @@
           <div class="email">{{ item.email }}</div>
         </div>
       </div>
-
+<!-- messages -->
       <div class="col-md-8 general-container">
-        here
+        <div class="single-message" v-bind:key="index" v-for="(item, index) in this.allMessages">
+          <div class="writer m-1">{{ item.user.name }}</div>
+          <div class="message m-1">{{ item.messages }}</div>
+        </div>
+
+        <form class="form">
+          <input 
+            name="message" 
+            tupe="text" 
+            class="form-control"
+            v-model="message"
+          />
+          <button class="btn btn-primary" @click="sendMessage">send</button>
+        </form>
       </div>
     </div>
   </div>
@@ -38,7 +52,10 @@ export default {
     return {
       users: null,
       noUserMessage: "",
-      authUser: ""
+      authUser: "",
+      message: [],
+      newMessage: [],
+      allMessages: []
     }
   },
 
@@ -95,12 +112,48 @@ export default {
             }
           })
           .catch((error) => {console.log(error)})
-    }
+    },
+
+    sendMessage (e) {
+      e.preventDefault();
+      this.axios
+          .post('http://localhost:8000/api/sendMessage', {data: this.message}, {
+            headers: {
+              'Authorization': `Bearer ${localStorage.getItem("token")}`
+            }
+          })
+          .then((response) => {
+              if(response.data !== "failure") {
+                this.message = ""
+                this.allMessages.push(response.data); 
+              }
+          })
+          .catch((error) => {console.log(error)})
+    },
+
+    getMessages () {
+      this.axios
+          .get('http://localhost:8000/api/get-messages', { 
+            headers: {
+              'Authorization': `Bearer ${localStorage.getItem("token")}`
+            }
+          })
+          .then((response) => {
+            if (response.data !== 'failure') {
+               this.allMessages = response.data;
+            }
+            else {
+              this.noUserMessage = "there is no users yet"
+            }
+          })
+          .catch((error) => {console.log(error)})
+    },
   },
 
   mounted () {
     this.getAuthUser(),
-    this.getUsers()
+    this.getUsers(),
+    this.getMessages()
   }
 }
 </script>
@@ -108,6 +161,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .users-list {
+     height: 70vh !important;
      display: flex;
      justify-content: center;
      flex-wrap: wrap;
@@ -115,12 +169,8 @@ export default {
      height: 400px;
      overflow: scroll;
   }
-  .general-container {
-     border: 1px solid;
-  }
   .user-item {
     overflow-x: hidden;
-
     padding: 3px;
     margin: 3px;
     width: 90%;
@@ -134,5 +184,33 @@ export default {
   .email {
     text-align: left;
     font-size: 14px;
+  }
+  .general-container {
+    flex-wrap: wrap;
+    height: 70vh !important;
+    overflow: scroll;
+    display: flex;
+    align-items: flex-start;
+    padding: 10px;
+    border: 1px solid;
+  }
+  .form {
+    width: 100%;
+    display: flex;
+    align-self: flex-end;
+  }
+  .single-message {
+    width: 100%;
+    border: 1px solid;
+    border-radius: 5px;
+    margin: 5px 0;
+  }
+  .writer {
+    text-align: left;
+     font-size: 11px ;
+  }
+  .message {
+    text-align: left;
+     font-size: 16px;
   }
 </style>
